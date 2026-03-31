18909601ba05042e9d60c52331bfcfe087c84f80
Reyajrk9 <reyajali9996@gmail.com>
2026-03-31 19:59:56 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import telebot
+
+TOKEN = "YOUR_BOT_TOKEN"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "🔥 Welcome to Trading Signal Bot!")
+
+@bot.message_handler(func=lambda message: True)
+def signal(message):
+    bot.reply_to(message, "📊 Signal: BUY NIFTY 🔥")
+
+bot.polling()

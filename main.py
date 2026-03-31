5102942bbb3a1c90454acc17da30284b5dc46d47
Reyajrk9 <reyajali9996@gmail.com>
2026-03-31 22:02:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import telebot
+import time
+import threading
+import random
 
 TOKEN = "8626210155:AAH914jJYsADPAU4ZuLK3gdaZiW611TAj5o"
-
+CHANNEL_ID = "@rkniftysignals"
 
 bot = telebot.TeleBot(TOKEN)
 
-CHANNEL_ID = "@rkniftysignals"
+def auto_signal():
+    while True:
+        signal = random.choice(["BUY NIFTY 🔥", "SELL NIFTY ⚠️"])
+        text = f"📊 Auto Signal: {signal}"
+        
+        bot.send_message(CHANNEL_ID, text)
+        time.sleep(300)  # 5 min
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "🔥 Welcome to Trading Signal Bot!")
+def start(message):
+    bot.reply_to(message, "🔥 RK Auto Trading Bot Active!")
 
-@bot.message_handler(func=lambda message: True)
-def signal(message):
-    bot.reply_to(message, "📊 Signal: BUY NIFTY 🔥")
+threading.Thread(target=auto_signal).start()
 
 bot.polling()
